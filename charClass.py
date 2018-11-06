a53a5efb075e80a1442ab846477f984947c6075c
Maatt Breck <matt.breck@live.com>
2018-11-06 15:54:34 -0500

    finished the barb class and tsted it

--- charClass.py
+++ charClass.py
@@ -33,11 +33,41 @@ class Fighter():
         return self
 
 class Barbarian():
-    def __init__(self,, hp, armor, weapons, )
+    def __init__(self,path, hp, armor, weapons,saving_throws, skills):
+        self.path=path
+        self.hp=hp
+        self.armor=armor
+        self.weapons=weapons
+        self.saving_throws=saving_throws
+        self.skills=[]
+    
+    def genPath(self):
+        pathList=["beserker", "totem warrior"]
+        self.path=random.choice(pathList)
+        return self
+    
+    def getSkills(self):
+        sList=["animal handling", "athletics", "intimidation", "nature", "perception", "survival"]
+        self.skills.append(random.choice(sList))
+        sList.remove(self.skills[0])
+        self.skills.append(random.choice(sList))
+        return self
 
 
 class Wizard():
-    def __init__(self, college, hp, armor,  weapon, cantrips, spells, spellslots)
+    def __init__(self, college, hp, armor, weapons, cantrips, spells, spell_slots, saving_throws, skills):
+        self.college=college
+        self.hp=hp
+        self.armor=armor
+        self.weapons=weapons
+        self.cantrips=[]
+        self.spells=[]
+        self.spell_slots=spell_slots
+        self.saving_throws=saving_throws
+        self.skills=[]
+
+
+
 class Bard():
     def __init__(self, college, hp, armor, weapons, instraments, saving_throws, inspiration, spells, spell_slots):
         self.college=college
@@ -56,7 +86,7 @@ class Bard():
         return self
     
     def getInstraments(self):
-        ilist["bagpipes", "drum", "dulcimer", "flute", "lute", "lyre", "horn", "pan flute", "shawm", "viol"]
+        ilist=["bagpipes", "drum", "dulcimer", "flute", "lute", "lyre", "horn", "pan flute", "shawm", "viol"]
         self.instraments=random.choice(ilist)
         return self
 
@@ -76,10 +106,21 @@ def makeFighter(f):
     f.getSkills()
     return f
 
+def makeBarb(b):
+    b.genPath()
+    b.getSkills()
+    return b
 
-fighter=Fighter('', '', '1d10', 'all, shields', 's,m', 'str,con',"")
+
+fighter=Fighter('', '', '1d10', 'all,shields', 's,m', 'str,con',"")
 
 made=makeFighter(fighter)
+
+barb=Barbarian('','1d12','l,m,sheilds','s,m','str,con','')
+made2=makeBarb(barb)
+
+print("fighter:",made.__dict__)
+print("barbarian",made2.__dict__)
 
 bard=Bard('','1d8','','','','','','','')
 bard.getSpells()
