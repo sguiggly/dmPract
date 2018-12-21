fa25f2113c778df50390fc461d42cad6ce362913
matt <matt.breck@live.com>
2018-12-20 20:24:49 -0500

    made connect 4 playable

--- tictactoe.py
+++ tictactoe.py
@@ -16,14 +16,85 @@ def get_player(turn):
         player=2
     return player
 
-def update_board(board, selection):
-    
+def update_board(board, selection, player):
+    row=5
+    while row>=0:
+        if board[row][selection]=='.':
+            if player==1:
+                board[row][selection]='x'
+            else:
+                board[row][selection]='o'
+            break
+        else:
+            row-=1
+    return board
+
+def is_valid(board, selection):
+    if board[0][selection]=='.':
+        return True
+    else:
+        return False
+
+def check_win(board,player):
+    boardHeight = len(board[0])
+    boardWidth = len(board)
+    if player==1:
+        tile='x'
+    else:
+        tile='o'
+    # check horizontal spaces
+    for y in range(boardHeight):
+        for x in range(boardWidth - 3):
+            if board[x][y] == tile and board[x+1][y] == tile and board[x+2][y] == tile and board[x+3][y] == tile:
+                return True
+
+    # check vertical spaces
+    for x in range(boardWidth):
+        for y in range(boardHeight - 3):
+            if board[x][y] == tile and board[x][y+1] == tile and board[x][y+2] == tile and board[x][y+3] == tile:
+                return True
+
+    # check / diagonal spaces
+    for x in range(boardWidth - 3):
+        for y in range(3, boardHeight):
+            if board[x][y] == tile and board[x+1][y-1] == tile and board[x+2][y-2] == tile and board[x+3][y-3] == tile:
+                return True
+
+    # check \ diagonal spaces
+    for x in range(boardWidth - 3):
+        for y in range(boardHeight - 3):
+            if board[x][y] == tile and board[x+1][y+1] == tile and board[x+2][y+2] == tile and board[x+3][y+3] == tile:
+                return True
+
+    return False
 
  
 def game(board):
+    print_board(board)
     done = False
     turn=True
     while not done:
         player=get_player(turn)
         if player==1:
-            selection=input('Player1 needs pick a column')
+            selection=int(input('Player1 needs pick a column'))
+            if not is_valid(board,selection):
+                selection=int(input('not a valid move, pick a new column'))
+            update_board(board,selection,player)
+            if check_win(board,player):
+                print('Player1 wins!')
+                done=True
+            else:
+                turn=False
+        else:
+            selection=int(input('Player2 needs pick a column'))
+            if not is_valid(board,selection):
+                selection=int(input('not a valid move, pick a new column'))
+            update_board(board,selection,player)
+            if check_win(board,player):
+                print('Player2 wins!')
+                done=True
+            else:
+                turn=True
+        print_board(board)
+
+game(board)
